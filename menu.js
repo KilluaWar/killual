615bc20486703db19d3d8fa12faa3bcc84eabb1f
KilluaWar <87344273+KilluaWar@users.noreply.github.com>
2021-07-14 19:01:16 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,160 @@
+const help = (p, date, user, wame) => {
+return `
+╔╦══• •✠•❀•✠ • •══╦╗
+║ comandos
+║
+║ 𝐁𝐎𝐓 : 「 𝑲𝑰𝑳⃟𝑳𝑼𝑨 ⃪𝑼𝑵𝑲𝑵⃟𝑶𝑾𝑵 」
+║
+║ 𝐏𝐑𝐄𝐅𝐈𝐗𝐎 : ${p}
+║
+║ 𝐃𝐎𝐍𝐎 : wa.me/+557598868032
+║
+║𝘿𝘼𝙏𝘼 𝙀 𝙃𝙊𝙍𝘼 : ${date} 
+║
+║𝙐𝙎𝙐𝘼𝙍𝙄𝙊 : ${user} 
+║
+║𝙒𝘼𝙈𝙀 : ${wame} 
+║
+╚╩══• •✠•❀•✠ • •══╩╝
+
+
+┏━━━⚡ 𝙋𝙍𝙄𝙉𝘾𝙄𝙋𝘼𝙄𝙎 ⚡━━━
+╟➤listblockcmd
+╟➤listpalavra
+╟➤info
+╟➤del
+╟➤delete
+╟➤comandos
+╟➤ping
+╟➤criador
+╽┷╼❥
+
+
+┏━━━━😎 𝘾𝙍𝙄𝘼𝘿𝙊𝙍 😎━━━━
+╟➤blockcmd (cmd sem prefix)
+╟➤unblockcmd (cmd sem prefix)
+╟➤alerta (txt)
+╟➤antinewchat (1 or 0)
+╟➤antispamcmd (1 or 0)
+╟➤antipv (1 or 0)
+╟➤antinewchat (1 or 0)
+╟➤infinityblock (1 or 0)
+╟➤antiligar (1 or 0)
+╟➤addpalavra (txt)
+╟➤removepalavra (txt)
+╟➤limparchat (txt)
+╟➤ataque (txt)
+╟➤divulgar (txt)
+╟➤entrargp (txt)
+╟➤block (num)
+╟➤unblock (num)
+╟➤msginfo
+╟➤chatinfo
+╽┷╼❥
+
+
+┏━━━━━❌ 𝘼𝙉𝙏𝙄 ❌━━━━━
+╟➤antidoc (1 or 0)
+╟➤antiloc (1 or 0)
+╟➤antiimg (1 or 0)
+╟➤antipalavra (1 or 0)
+╟➤antivideo (1 or 0)
+╟➤antisticker (1 or 0)
+╟➤antiaudio (1 or 0)
+╟➤antictt (1 or 0)
+╟➤antilink (1 or 0)
+╟➤antiporn (1 or 0)
+╟➤antifake (1 or 0)
+╟➤dontback (1 or 0)
+╟➤antilinkhard (1 or 0)
+╽┷╼❥
+
+
+┏━━━━👥 𝙂𝙍𝙐𝙋𝙊𝙎 👥━━━
+╟➤listfake
+╟➤listbr
+╟➤listddd (country code)
+╟➤dbackadd (num)
+╟➤dbackrm (num)
+╟➤fechargp
+╟➤mudardesc
+╟➤mudarnome
+╟➤abrirgp
+╟➤linkgroup
+╟➤listadmin
+╟➤listonline
+╟➤leave
+╟➤kick (dial num)
+╟➤ban (resp msg)
+╟➤promote (dial num)
+╟➤demote (dial num)
+╟➤add (num)
+╟➤roletrussablock (1 or 0)
+╟➤welcome (1 or 0)
+╟➤simih (1 or 0)
+╟➤autostickerimg (1 or 0)
+╟➤onmodgrupo
+╟➤offmodgrupo
+╟➤autoreply (1 or 0)
+╟➤marcar
+╟➤marcar2
+╟➤marcar3
+╟➤hidemarcar (txt)
+
+
+┏━━━━✨ 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 ✨━━━━╮
+╟➤stiker (leg, resp img)
+╟➤sticker (leg, resp img)
+╟➤csticker (leg, resp img)
+╟➤cstiker (leg, resp img)
+╟➤fstiker (leg, resp img)
+╟➤fsticker (leg, resp img)
+╟➤nstiker (pack) (author)
+╟➤nsticker (pack) (author)
+╟➤nfstiker (pack) (author)
+╟➤nfsticker (pack) (author)
+╟➤macaco
+╽┷╼❥
+
+
+┏━━━━━🎵 𝘼𝙐𝘿𝙄𝙊 🎵━━━
+╟➤shazam (resp audio)
+╟➤gtts (la) (txt)
+╟➤playlist
+╟➤lyrics
+╟➤ptlyrics
+╟➤play (music name)
+╟➤playv2 (music name)
+╟➤tomp3 (cap, resp vid)
+╟➤ytsearch (name)
+╟➤ytsrc (name)
+╟➤ytmp4 (yt url)
+╟➤ytmp3 (yt url)
+╽┷╼❥
+
+
+┏━━━━━🇯🇵 𝘼𝙉𝙄𝙈𝙀𝙎 🇯🇵━━━━
+╟➤randomanime
+╟➤randomshota
+╟➤randomkiss
+╟➤randomcry
+╟➤randomhug
+╟➤nekoanime
+╟➤wait (cap, resp vid)
+╟➤slap
+╟➤smile
+╟➤piscada
+╟➤dance
+╟➤cringe
+╟➤blush
+╟➤gore
+╟➤acenar
+╟➤lick
+╟➤bully
+╟➤kemonomimi
+╽┷╼❥`
+}
+
+exports.help = help
+
+
